b952bde951c1387075126df74cf1214db6deb3e0
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 04:14:40 -0400

    Modelo Tag configurado
    - Se dieron de alta los campos fillables
    - Se definió la relación muchos a muchos con posts
    - Se usó el método withTimestamps para conservar la fecha en la tabla pivot

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    /*********** Campos de clase ***********/
+    protected $fillable = [
+        'name', 'slug',
+    ];
+
+    /*********** Relación uno a muchos ***********/
+    public function posts()
+    {
+        return $this->belongsToMany('App\Post')->withTimestamps();
+    }
 }
