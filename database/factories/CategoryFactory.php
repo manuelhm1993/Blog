6cdf3074c106e7a94cd3af90e3409ff0aa6d529d
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 02:30:59 -0400

    Lógica del CategoryFactory
    - Se importó la clase Category para sustituir a la clase Model
    - Se importó la clase Str para poder generar un slug a partir del nombre de la categoría
    - Se utilizó el objeto $faker para generar tanto una sentencia como un texto

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Category;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+use Illuminate\Support\Str;//Se debe importar la clase Str para usar el método slug
+
+$factory->define(Category::class, function (Faker $faker) {
+    $title = $faker->sentence(4);
+    
+    //$slug = Str::slug($title, '-');//Categoría Alimentos -> categoria-alimentos
+    
+    $slug = Str::of($title)->slug('-');//Sintaxis alternativa
+
     return [
-        //
+        'name' => $title,
+        'slug' => $slug,
+        'body' => $faker->text(500),
     ];
 });
