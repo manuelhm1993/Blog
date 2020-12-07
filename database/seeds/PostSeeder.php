42fbe193bc6fec9b13f9c71af643c4b7a265e74a
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 03:57:58 -0400

    Relación muchos a muchos laravel
    - Se creó la relación en laravel utilizando el método tags en el modelo Post, esto permite adjuntar la relación muchos a muchos entre posts y tags en la tabla pivot
    - Se utilizó el método each usando métodos encadenados para crear la relación de cada post con sus respectivas etiquetas

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -11,6 +11,13 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 300)->create();
+        factory(App\Post::class, 300)->create()->each(function ($post) {
+            //Relacionar cada post con tres etiquetas de forma aleatoria
+            $post->tags()->attach([
+                rand(1, 5),
+                rand(6, 14),
+                rand(15, 20),
+            ]);
+        });
     }
 }
