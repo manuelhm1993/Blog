b04a6ac799e6966412b152d409949862c167dd94
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 04:17:45 -0400

    Definir usuario -> posts
    Se dio de alta la relación uno a muchos de usuarios y posts en el modelo User

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*********** Relación uno a muchos ***********/
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
