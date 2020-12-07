183aef2874911c3138344f9940372de45369f837
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 04:03:49 -0400

    Relaciones posts y asignación masiva
    - Se definieron las relaciones uno a muchos inversa de:
    - posts <- user
    - posts <- category
    - Se utilizó la propiedad $fillable para indicar los campos que permiten asignación masiva

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    /*********** Campos de clase ***********/
+    protected $fillable = [
+        'user_id', 'category_id', 'name',
+        'slug', 'excerpt', 'body',
+        'status', 'file',
+    ];
+
+    /*********** Relación uno a muchos inversa ***********/
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    /*********** Relación muchos a muchos ***********/
     public function tags()
     {
         return $this->belongsToMany('App\Tag')->withTimestamps();
