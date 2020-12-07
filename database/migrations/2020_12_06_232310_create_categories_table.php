310cacc95df207e07dba061fbffb27f8b83a75f0
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-06 21:07:52 -0400

    Tabla categorias lista
    - Se definieron todos los campos de la tabla
    - Se definió un slug para poder trabajar con URLs Friendly
    - Se ejecutó la migración con el atributo refresh para aplicar los cambios

--- database/migrations/2020_12_06_232310_create_categories_table.php
+++ database/migrations/2020_12_06_232310_create_categories_table.php
@@ -15,6 +15,16 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            /**
+             * Slug significa babosa, se caracteriza por dejar su rastro al moverse
+             * El término le queda perfecto ya que permite crear URLs Friendly
+             * Si no se usan los slugs al ver un post la url queda: http://mhenriquez/post/1
+             * Para hacer SEO la url debe referenciar el contenido: http://mhenriquez/post/desarrollo-web
+             * Esto es un slug
+             */
+            $table->string('slug')->unique();
+            $table->mediumText('body')->nullable();
             $table->timestamps();
         });
     }
