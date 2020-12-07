a3f12ebe6572fe8e8578b371aa14fe89f922c114
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-07 04:10:35 -0400

    Configurado el modelo Cateogry
    - Se dieron de alta los campos fillables
    - Se definió la relación uno a muchos con posts

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /*********** Campos de clase ***********/
+    protected $fillable = [
+        'name', 'slug', 'body',
+    ];
+
+    /*********** Relación uno a muchos ***********/
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
