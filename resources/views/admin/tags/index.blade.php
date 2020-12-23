70d3e07109e030b270337b3b8bb3832bb6d3b3b7
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-23 19:17:52 -0400

    Se creó el contenido dinámico
    Se utilizó un foreach para cargar dinámicamente el contenido

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -14,12 +14,30 @@
                         <table class="table table-striped table-hover">
                             <thead>
                                 <tr>
-                                    
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col" colspan="3">Acciones</th>
                                 </tr>
                             </thead>
 
                             <tbody>
-                                
+                                @foreach ($tags as $tag)
+                                    <tr>
+                                        <td>{{ $tag->id }}</td>
+                                        <td>{{ $tag->name }}</td>
+                                        <td width="10px">
+                                            <a href="{{ route('admin.tags.show', $tag->id) }}" class="btn btn-sm btn-secondary">Ver</a>
+                                        </td>
+
+                                        <td width="10px">
+                                            <a href="{{ route('admin.tags.edit', $tag->id) }}" class="btn btn-sm btn-secondary">Editar</a>
+                                        </td>
+
+                                        <td width="10px">
+                                            Eliminar
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
