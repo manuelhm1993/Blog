0e9c5a951bc1d6dd622d6cada18d26c8c398971a
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-23 23:17:49 -0400

    Campos de formulario creados
    Se definió el include para crear los campos del formulario

--- resources/views/admin/tags/includes/form.blade.php
+++ resources/views/admin/tags/includes/form.blade.php
@@ -1,1 +1,19 @@
-Formulario
+<div class="form-group">
+    {!! Form::label('name', 'Nombre de la etiqueta') !!}
+    {!! Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('slug', 'URL Amigable') !!}
+    {!! Form::text('slug', null, ['class' => 'form-control', 'id' => 'slug']) !!}
+</div>
+
+@if (isset($tag))
+    <div class="form-group">
+        {!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+    </div>
+@else
+    <div class="form-group">
+        {!! Form::submit('Crear', ['class' => 'btn btn-primary']) !!}
+    </div>
+@endif
