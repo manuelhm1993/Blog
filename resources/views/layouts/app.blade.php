4ede52902307d0f8e7f1901eb719b61daf31d99c
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-23 21:48:54 -0400

    Feedback de errores
    Se utilizó la misma estructura de los feedback de éxito, pero utilizando el objeto $errors y todos sus métodos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,6 +104,28 @@
                 </div>
             @endif
 
+            @if ($errors->any())
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 offset-md-2">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>Error: </strong>
+
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+            
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
             @yield('content')
         </main>
     </div>
