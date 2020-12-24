aa74d3c72262f6171d9e10f26bed72e768c52d68
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-24 17:23:18 -0400

    Vista show terminada
    Se hicieron las modificaciones pertinentes en la vista

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,20 +6,20 @@
             <div class="col-md-8 offset-md-2">
                 <div class="card">
                     <h5 class="card-header">
-                        Detalle de la categoría
+                        Detalle de la entrada
                     </h5>
 
                     <div class="card-body">
                         <p class="card-text">
-                            <strong>Nombre: </strong> {{ $category->name }}
+                            <strong>Nombre: </strong> {{ $post->name }}
                         </p>
 
                         <p class="card-text">
-                            <strong>Slug: </strong> {{ $category->slug }}
+                            <strong>Slug: </strong> {{ $post->slug }}
                         </p>
 
                         <p class="card-text">
-                            <strong>Contenido: </strong> {{ $category->body }}
+                            <strong>Contenido: </strong> {{ $post->body }}
                         </p>
                     </div>
                 </div>
